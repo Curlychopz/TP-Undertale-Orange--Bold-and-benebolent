650e2b5ad805c817f858d191c0816d7f0099c692
Curlychopz <123845936+Curlychopz@users.noreply.github.com>
2024-12-29 02:01:31 +0000

    procrastinated
    job for tomorrow

--- scenes/misc/scene_switcher.gd
+++ scenes/misc/scene_switcher.gd
@@ -10,7 +10,8 @@ var current_room = null
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	#$logo.connect("level_changed", self, "handle_level_changed")
-	get_window().mode = Window.MODE_FULLSCREEN
+	#get_window().mode = Window.MODE_FULLSCREEN
+	pass
 
 func handle_level_changed(current_level_name):
 	pass
@@ -19,13 +20,13 @@ func handle_level_changed(current_level_name):
 func _process(delta):
 	pass
 
-var scene_index_to_change = -1 # we leave it at -1 at first when we want it to stay idle.
-var scene_switch_delay_start = 0.3
-var scene_switch_delay_end = 0.3
+#var scene_index_to_change = -1 # we leave it at -1 at first when we want it to stay idle.
+#var scene_switch_delay_start = 0.3
+#var scene_switch_delay_end = 0.3
 
-func scene_switch_process(delta):
-	if scene_index_to_change != -1:
-		current_room.hide()
+#func scene_switch_process(delta):
+#	if scene_index_to_change != -1:
+#		current_room.hide()
 		
 		
 		
